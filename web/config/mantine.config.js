59c9cf3c1e02799cdd1b187855f9f546b999b7f0
David Price <thedavid@thedavidprice.com>
2023-06-19 13:06:41 -0700

    codemod config fix for vite

--- web/config/mantine.config.js
+++ web/config/mantine.config.js
@@ -1,3 +1,6 @@
 // This object will be used to override Mantine theme defaults.
 // See https://mantine.dev/theming/mantine-provider/#theme-object for theming options
-module.exports = {}
+const theme = {
+  /**....your theme **/
+}
+export default theme
